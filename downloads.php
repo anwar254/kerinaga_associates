9a44e373fb6db7167a911365d8a9a74bdd6d1140
Anwar Magaraa <sadatt254@gmail.com>
2018-09-25 06:18:10 -0700

    done with the download page

---
+++ downloads.php
@@ -0,0 +1,41 @@
+<?php  
+	include_once 'header.php';
+?>
+<section class="hero about-hero" id="about-hero">
+  <div class="overlay"></div>
+  <div class="heading hero-text-container">
+    <h1 class="title k-text-center k-text-title">download</h1>
+  </div>
+</section>
+<section class="section">
+	<div class="container">
+		<div class="heading">
+			<div class="title">Download</div>
+			<div class="k-title-line"></div>
+			<p>Written by Administrator</p>
+		</div>
+		<div class="download-container">
+			<h4 class="k-capitalized">Dear, customer</h4>
+			<p>Most of the downloads are only available to our registered members. If you want to download our projects please <a href="contactus.php">contact us</a></p>
+
+			<div class="k-download-buttons">
+				<div class="columns">
+					<div class="column">
+						<div class="download-link">
+							<a href="http://keringassociates.co.ke/index.php?option=com_simpledownload&task=download&fileid=ZG9jcy5kb2M=" download="Doc">Text-To-Be-Displayed</a>
+						</div>
+					</div>
+					<div class="column">
+						<div class="download-link">
+							<a href="http://keringassociates.co.ke/index.php?option=com_simpledownload&task=download&fileid=ZG9jcy5kb2M=" download="Doc">Text-To-Be-Displayed</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php  
+	include_once 'footer.php';
+?>
